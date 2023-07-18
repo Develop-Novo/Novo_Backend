518a11addf2728f676a594eeaad5c0fa0b722625
jwPark <1211abc@naver.com>
2023-07-19 01:54:12 +0900

    project init

---
+++ src/test/java/novo/backend_novo/BackendNovoApplicationTests.java
@@ -0,0 +1,13 @@
+package novo.backend_novo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
