6318e5308bec63b2f92e15614be6f1c816f8d494
jwPark <1211abc@naver.com>
2023-07-22 19:48:32 +0900

    feat: content controller 구현

---
+++ src/main/java/novo/backend_novo/Controller/ContentController.java
@@ -0,0 +1,84 @@
+package novo.backend_novo.Controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import novo.backend_novo.Domain.Content;
+import novo.backend_novo.Response.BasicResponse;
+import novo.backend_novo.Response.CommonResponse;
+import novo.backend_novo.Service.ContentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static novo.backend_novo.DTO.CommonDTO.*;
+import static novo.backend_novo.DTO.ContentDTO.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/content")
+public class ContentController {
+    private final ContentService contentService;
+
+    /*작품 등록*/
+    @PostMapping(path = "/new",produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<? extends BasicResponse> save(
+            @RequestBody @Valid SaveRequest request
+    ){
+        String keyword = "";
+        for(String key : request.getKeyword())
+            keyword += key+",";
+
+        Content content = Content.builder()
+                .title(request.getTitle())
+                .writer(request.getWriter())
+                .introduction(request.getIntroduction())
+                .price(request.getPrice())
+                .serialDay(request.getSerialDay())
+                .publishedAt(request.getPublishedAt())
+                .genre(request.getGenre())
+                .keyword(keyword)
+                .ageRating(request.getAgeRating())
+                .platform(request.getPlatform())
+                .build();
+        return ResponseEntity.ok()
+                .body(new CommonResponse<IdResponse>(contentService.saveContent(content)));
+    }
+
+    /*작품 정보 id로 조회*/
+    @GetMapping(path = "/id/{content-id}",produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<? extends BasicResponse> findById(
+            @PathVariable("content-id") Long contentId
+    ){
+        return ResponseEntity.ok()
+                .body(new CommonResponse<InfoResponse>(contentService.getContentInfoWithId(contentId)));
+    }
+
+    /*작품 정보 전체 조회*/
+    @GetMapping(path = "/all",produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<? extends BasicResponse> findAll()
+    {
+        return ResponseEntity.ok()
+                .body(new CommonResponse<List>(contentService.getAllContents()));
+    }
+
+    /*작품 수정*/
+    @PutMapping(path = "/id/{content-id}",produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<? extends BasicResponse> update(
+            @PathVariable("content-id") Long contentId,
+            @RequestBody @Valid UpdateRequest request
+    ){
+        contentService.update(contentId,request);
+        return ResponseEntity.ok().build();
+    }
+
+    /*작품 삭제*/
+    @DeleteMapping(path = "/id/{content-id}", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<? extends BasicResponse> remove(
+            @PathVariable("content-id") Long contentId
+    ){
+        contentService.removeContent(contentId);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/novo/backend_novo/DTO/ContentDTO.java
@@ -0,0 +1,72 @@
+package novo.backend_novo.DTO;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import novo.backend_novo.Domain.Content;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+public class ContentDTO {
+
+    @Getter @Setter
+    public static class SaveRequest{
+        @NotEmpty
+        private String title;
+        private String writer;
+        private String introduction;
+        private String price;
+        private String serialDay;
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        private LocalDate publishedAt;
+        private String genre;
+        private List<String> keyword;
+        private String ageRating = "전체이용가";
+        private String platform;
+    }
+
+    @Getter @Setter
+    public static class UpdateRequest{
+        private String title;
+        private String writer;
+        private String introduction;
+        private String price;
+        private String serialDay;
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        private LocalDate publishedAt;
+        private String genre;
+        private List<String> keyword;
+        private String ageRating = "전체이용가";
+        private String platform;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class InfoResponse{
+        private Long id;
+        private String title;
+        private String writer;
+        private String introduction;
+        private String price;
+        private String serialDay;
+        private LocalDate publishedAt;
+        private String genre;
+        private List<String> keyword;
+        private String ageRating;
+        private String platform;
+
+        public static InfoResponse of(Content content){
+            List<String> keywords = Arrays.asList(content.getKeyword().split(","));
+
+            return new InfoResponse(content.getId(),content.getTitle(), content.getWriter(), content.getIntroduction(),
+                    content.getPrice(), content.getSerialDay(), content.getPublishedAt(), content.getGenre(), keywords,
+                    content.getAgeRating(), content.getPlatform());
+        }
+    }
+}
